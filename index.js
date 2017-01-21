9c04fca62ac6bd8b6c24deab92eef5887efcb4fa
themakeshack <themakeshack@gmail.com>
2017-01-21 14:54:07 -0800

    First commit

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by themakeshack on 1/21/17.
+ */
+var fs = require("fs");
+
+fs.readFile('input.txt', function (err, data) {
+    if (err) return console.error(err);
+    console.log(data.toString());
+});
+
+console.log("Program Ended");
