2d5881beb0dd8b12d460d6935c92ad73f5a42e41
themakeshack <themakeshack@gmail.com>
2017-01-21 15:11:58 -0800

    First commit

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var fs = require("fs");
 fs.readFile('input.txt', function (err, data) {
     if (err) return console.error(err);
     console.log(data.toString());
+    console.log("Program is ending inside the callback!");
 });
 
 console.log("Program Ended");
